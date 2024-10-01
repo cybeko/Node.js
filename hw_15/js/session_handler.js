ab10443e6507e72e6825fda9c78cbf88686036d0
Каланжова Анна Олександрівна <Kala_q1tb>
2024-10-01 08:59:36 +0300

    initial

---
+++ hw_15/js/session_handler.js
@@ -0,0 +1,17 @@
+﻿var mssql = require('mssql');
+
+var dbConfig = {
+    user: 'user32',
+    password: 'hello',
+    server: '28-2',
+    database: 'testdb',
+    port: 1433,
+    options: {
+        encrypt: true,
+        trustServerCertificate: true
+    }
+};
+
+module.exports = {
+    dbConfig
+};
